1bcd67dd239edc2f8c136f33d48f2aac74e64a07
kpurcell <github@kevin-purcell.com>
2014-02-13 09:58:34 -0500

    commented out the debugging tool in divMetrics

--- functions/divMetrics.R
+++ functions/divMetrics.R
@@ -4,7 +4,7 @@
 # dependencies: Vegan package
 
 divMetrics<- function(x){
-  browser()
+  #browser()
   N<-rowSums(x) # total individuals caught per station
   shan<- diversity(x, index="shannon") #shannon
   simp<-diversity(x, index="simpson") #simpson
