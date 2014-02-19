4346c54fdf4634642f951096d94edc0b1e24a048
kpurcell <github@kevin-purcell.com>
2014-02-19 12:33:55 -0500

    updates to figure 3 adding point data

--- Analysis/ResultsFigures/figure3.R
+++ Analysis/ResultsFigures/figure3.R
@@ -17,71 +17,86 @@ plot(sum.la.agg2$S.norm~sum.la.agg2$YR, pch="",
      main="Louisiana Summer",
      xlab="Year",
      ylab="Normalized value",
-     ylim=c(0.5,1.2))
-#points(sum.la.agg2$N.norm.mean~sum.la.agg2$YR, type="p", pch=2)
+     ylim=c(0.6,1.2))
+rect(2000,0.6,2004,1.2, col="grey", border="transparent")
+points(sum.la.agg2$d.norm~sum.la.agg2$YR, type="p", pch=15)
+points(sum.la.agg2$J.norm~sum.la.agg2$YR, type="p", pch=16)
+points(sum.la.agg2$shan.norm~sum.la.agg2$YR, type="p", pch=17)
+points(sum.la.agg2$simp.norm~sum.la.agg2$YR, type="p", pch=18)
+
+
 lines(lowess(sum.la.agg2$d.norm~sum.la.agg2$YR), lty=1, col=26, lwd=1.8)
 lines(lowess(sum.la.agg2$J.norm~sum.la.agg2$YR), lty=2, col=51, lwd=1.8)
 lines(lowess(sum.la.agg2$shan.norm~sum.la.agg2$YR), lty=3, col=76, lwd=1.8)
 lines(lowess(sum.la.agg2$simp.norm~sum.la.agg2$YR), lty=4, col=101, lwd=1.8)
-lines(lowess(sum.la.agg2$N1.norm~sum.la.agg2$YR), lty=5, col=126, lwd=1.8)
-lines(lowess(sum.la.agg2$N2.norm~sum.la.agg2$YR), lty=6, col=151, lwd=1.8)
+#lines(lowess(sum.la.agg2$N1.norm~sum.la.agg2$YR), lty=5, col=126, lwd=1.8)
+#lines(lowess(sum.la.agg2$N2.norm~sum.la.agg2$YR), lty=6, col=151, lwd=1.8)
 grid(col="lightgrey")
+# abline(v=2003,col=4,lty=2)
+# abline(v=1996,col=9,lty=2) #hypoxia
+ 
 
 #la fall
 plot(fall.la.agg2$S.norm~fall.la.agg2$YR, pch="",
      main="Louisiana Fall",
      xlab="Year",
      ylab="Normalized value",
-     ylim=c(0.2,1.5))
+     ylim=c(0.6,1.5))
+rect(2000,0,2004,2, col="grey", border="transparent") 
+
+points(fall.la.agg2$d.norm~fall.la.agg2$YR, type="p", pch=0)
+points(fall.la.agg2$J.norm~fall.la.agg2$YR, type="p", pch=1)
+points(fall.la.agg2$shan.norm~fall.la.agg2$YR, type="p", pch=2)
+points(fall.la.agg2$simp.norm~fall.la.agg2$YR, type="p", pch=5)
 #points(fall.la.agg2$N.norm.mean~fall.la.agg2$YR, type="p", pch=2)
+
 lines(lowess(fall.la.agg2$d.norm~fall.la.agg2$YR), lty=1, col=26, lwd=1.8)
 lines(lowess(fall.la.agg2$J.norm~fall.la.agg2$YR), lty=2, col=51, lwd=1.8)
 lines(lowess(fall.la.agg2$shan.norm~fall.la.agg2$YR), lty=3, col=76, lwd=1.8)
 lines(lowess(fall.la.agg2$simp.norm~fall.la.agg2$YR), lty=4, col=101, lwd=1.8)
-lines(lowess(fall.la.agg2$N1.norm~fall.la.agg2$YR), lty=5, col=126, lwd=1.8)
-lines(lowess(fall.la.agg2$N2.norm~fall.la.agg2$YR), lty=6, col=151, lwd=1.8)
+#lines(lowess(fall.la.agg2$N1.norm~fall.la.agg2$YR), lty=5, col=126, lwd=1.8)
+#lines(lowess(fall.la.agg2$N2.norm~fall.la.agg2$YR), lty=6, col=151, lwd=1.8)
 grid(col="lightgrey")
 # abline(v=2003,col=4,lty=2)
 # abline(v=1996,col=9,lty=2) #hypoxia
-# rect(1998,0,2003,2, density=20, col="grey",
-#      angle=-30, border="transparent") 
+
 
 #tx summer
 plot(sum.tx.agg2$S.norm~sum.tx.agg2$YR, pch="",
      main="Texas Summer",
      xlab="Year",
      ylab="Normalized value",
-     ylim=c(0.6,1.2))
+     ylim=c(0.9,1.15))
 #points(sum.tx.agg2$N.norm.mean~sum.tx.agg2$YR, type="p", pch=2)
 lines(lowess(sum.tx.agg2$d.norm~sum.tx.agg2$YR), lty=1, col=26, lwd=1.8)
 lines(lowess(sum.tx.agg2$J.norm~sum.tx.agg2$YR), lty=2, col=51, lwd=1.8)
 lines(lowess(sum.tx.agg2$shan.norm~sum.tx.agg2$YR), lty=3, col=76, lwd=1.8)
 lines(lowess(sum.tx.agg2$simp.norm~sum.tx.agg2$YR), lty=4, col=101, lwd=1.8)
-lines(lowess(sum.tx.agg2$N1.norm~sum.tx.agg2$YR), lty=5, col=126, lwd=1.8)
-lines(lowess(sum.tx.agg2$N2.norm~sum.tx.agg2$YR), lty=6, col=151, lwd=1.8)
+#lines(lowess(sum.tx.agg2$N1.norm~sum.tx.agg2$YR), lty=5, col=126, lwd=1.8)
+#lines(lowess(sum.tx.agg2$N2.norm~sum.tx.agg2$YR), lty=6, col=151, lwd=1.8)
 grid(col="lightgrey")
 # abline(v=2003,col=4,lty=2)
 # abline(v=1996,col=9,lty=2) #hypoxia
-# rect(1999,0,2003,2, density=20, col="grey",
-#      angle=-30, border="transparent") 
+rect(2002,0,2003,2, density=20, col="grey",
+     angle=-30, border="transparent") 
 
 #tx fall
 plot(fall.tx.agg2$S.norm~fall.tx.agg2$YR, pch="",
      main="Texas Fall",
      xlab="Year",
      ylab="Normalized value",
-     ylim=c(0.6,1.2))
+     ylim=c(0.9,1.1))
 #points(fall.tx.agg2$N.norm.mean~fall.tx.agg2$YR, type="p", pch=2)
 lines(lowess(fall.tx.agg2$d.norm~fall.tx.agg2$YR), lty=1, col=26, lwd=1.8)
 lines(lowess(fall.tx.agg2$J.norm~fall.tx.agg2$YR), lty=2, col=51, lwd=1.8)
 lines(lowess(fall.tx.agg2$shan.norm~fall.tx.agg2$YR), lty=3, col=76, lwd=1.8)
 lines(lowess(fall.tx.agg2$simp.norm~fall.tx.agg2$YR), lty=4, col=101, lwd=1.8)
-lines(lowess(fall.tx.agg2$N1.norm~fall.tx.agg2$YR), lty=5, col=126, lwd=1.8)
-lines(lowess(fall.tx.agg2$N2.norm~fall.tx.agg2$YR), lty=6, col=151, lwd=1.8)
+#lines(lowess(fall.tx.agg2$N1.norm~fall.tx.agg2$YR), lty=5, col=126, lwd=1.8)
+#lines(lowess(fall.tx.agg2$N2.norm~fall.tx.agg2$YR), lty=6, col=151, lwd=1.8)
 grid(col="lightgrey")
 # abline(v=2003,col=4,lty=2)
 # abline(v=1996,col=9,lty=2) #hypoxia
-# rect(1998,0,2005,2, density=20, col="grey",
-#      angle=-30, border="transparent") 
+rect(1995,0,2000,2, density=20, col="grey",
+     angle=-30, border="transparent") 
 
 #dev.off()
